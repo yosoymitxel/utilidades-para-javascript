ee976fea26d46ebf3aee752c8a795c36f6356fdc
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2019-11-15 14:33:38 -0300

    Update conseguir-numeros-de-un-string.js

--- conseguir-numeros-de-un-string.js
+++ conseguir-numeros-de-un-string.js
@@ -21,3 +21,32 @@ numero = conseguirNumeroString(textoConNumeros0);
 echo('El numero es: ('+numero+')');
 numero = conseguirNumeroString(textoConNumeros1);
 echo('El numero es: ('+numero+')');
+
+
+//BONUS, mostrar con unidades de mil ejemplo 10000 = 10.000
+function number_format(user_input){
+        var filtered_number = user_input.replace(/[^0-9]/gi, '');
+        var length = filtered_number.length;
+        var breakpoint = 1;
+        var formated_number = '';
+
+        for(i = 1; i <= length; i++){
+            if(breakpoint > 3){
+                breakpoint = 1;
+                formated_number = '.' + formated_number;
+            }
+            var next_letter = i + 1;
+            formated_number = filtered_number.substring(length - i, length - (i - 1)) + formated_number;
+
+            breakpoint++;
+        }
+
+        return formated_number;
+    }
+echo(number_format(numero));
+
+//BONUS 2 convertir a entero
+function entero(numero){
+    return parseInt(numero);
+}
+echo(entero(numero));
