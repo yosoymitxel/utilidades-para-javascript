ccf42842d4c018aee6093ec8d7fa7b83577f4e4b
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2021-10-28 09:47:58 -0300

    Create añadir-jquery-con-js.js

---
+++ añadir-jquery-con-js.js
@@ -0,0 +1,4 @@
+var script  = document.createElement('script');
+script.src  = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
