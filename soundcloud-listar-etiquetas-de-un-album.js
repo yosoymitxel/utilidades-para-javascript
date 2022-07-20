60b0f4438645dfc4556df15676e895f5a4ec3a3a
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-07-20 09:06:16 -0400

    Create soundcloud-listar-etiquetas-de-un-album.js

---
+++ soundcloud-listar-etiquetas-de-un-album.js
@@ -0,0 +1,14 @@
+function listarEtiquetasDeUnAlbum(){
+  var script  = document.createElement('script');
+  script.src  = 'https://code.jquery.com/jquery-3.4.1.min.js';
+  script.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(script);
+
+  var etiquetas = ''
+  $('.sc-truncate.sc-tagContent').each(function(a,e){
+      etiquetas += ($(e).text())+'\n'
+  })
+  console.log(etiquetas)
+}
+
+listarEtiquetasDeUnAlbum()
