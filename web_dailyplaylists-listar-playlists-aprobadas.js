da1acccb82ec9c904c357c3db64b18435dda6d53
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-11-15 09:20:30 -0300

    Update web_dailyplaylists-listar-playlists-aprobadas.js

--- web_dailyplaylists-listar-playlists-aprobadas.js
+++ web_dailyplaylists-listar-playlists-aprobadas.js
@@ -1,5 +1,10 @@
 //https://dailyplaylists.com/dashboard/
 
+var script  = document.createElement('script');
+script.src  = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 var arrPlaylist = []
 $('.Text-vnjkjx-0.dLFVil').each((a,e) => {
         if($(e).text() == 'approved'){
