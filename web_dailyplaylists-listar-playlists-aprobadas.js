1eb54e687d9587ecec67dc87d10098ce059336a6
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-11-21 10:31:45 -0300

    Update web_dailyplaylists-listar-playlists-aprobadas.js

--- web_dailyplaylists-listar-playlists-aprobadas.js
+++ web_dailyplaylists-listar-playlists-aprobadas.js
@@ -27,3 +27,24 @@ $('.Text-vnjkjx-0.dLFVil').each((a,e) => {
 console.log(arrPlaylistName.join('\n'))  
 console.log(arrPlaylistLinks.join('\n'))  
 console.log(arrPlaylist.join('\n'))  
+
+/*
+All Style Songs #2 - MORISDOWN - https://open.spotify.com/playlist/3gFyfvFYHRjieQulH7MoZX
+Support Musicians #6 - MORISDOWN - https://open.spotify.com/playlist/1R2IyiZtaHPoRPjcMPtNM9
+Approved All Style - MORISDOWN - https://open.spotify.com/playlist/3wUM9WjfTE5lZGFI1K3r0G
+SAFE DREAM - PlaylistCuratorSotify 1 - https://open.spotify.com/playlist/2WCUirVkpNUrFpwakSI7uI
+CONFORT - PlaylistCuratorSotify 1 - https://open.spotify.com/playlist/59grxtTXZj3xOMf1aeX9O3
+SWEET DREAMS - PlaylistCuratorSotify 1 - https://open.spotify.com/playlist/1LBjOhT9joswlOr7IyLNDg
+Sunset n Chill - MultiVibe Music - https://open.spotify.com/playlist/477ta1pt5IQzkNqxuksSnD
+Independent Artists Unite! - ADAD Audio - https://open.spotify.com/playlist/1QRG7PM4QmRnOgHy2doy7V
+The Space Race - Anything's Possible Music - https://open.spotify.com/playlist/01R1NFSVTUWInUvMd8oT2S
+GOAT Visions & Illusions - BoBoDred - https://open.spotify.com/playlist/2ra1dDLi09UZMiMS4UrwXt
+Lofi - fusionostalgia - https://open.spotify.com/playlist/4OsAMyuLw6geQdHegKQcv9
+LOFI - deathordesire - https://open.spotify.com/playlist/57Hwp02uVOvlbCW353lCLe
+2022 | Independent Artists Assemble - Steve Marks - https://open.spotify.com/playlist/3EU2Ct76DoEqf7zLKnJKA0
+Dragon: Urban & EDM - Julius Beat - https://open.spotify.com/playlist/6pVW4LBqWfzROLVyL78KGA
+Modern Pop Music Artists / Popular Modern Rock Artists / The Best Electronic Dance Music - henderson - https://open.spotify.com/playlist/333awHnNYIxcJMFeQOGYax
+Electronica - Loft Music - https://open.spotify.com/playlist/05kX1IpGRnYYroLg7Dl2oQ
+R&B - deathordesire - https://open.spotify.com/playlist/0XlNL2wMJF7iw0lrVkpTfb
+
+*/
