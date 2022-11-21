786ea9733e45f81a93723b70fabb05907f27fb33
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-11-21 09:58:24 -0300

    Update web_dailyplaylists-listar-playlists-aprobadas.js

--- web_dailyplaylists-listar-playlists-aprobadas.js
+++ web_dailyplaylists-listar-playlists-aprobadas.js
@@ -5,18 +5,25 @@ script.src  = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-var arrPlaylist = []
+var arrPlaylist      = []
+var arrPlaylistName  = []
+var arrPlaylistLinks = []
+
 $('.Text-vnjkjx-0.dLFVil').each((a,e) => {
         if($(e).text() == 'approved'){
             let nombres = $(e).parent().parent().parent().find('.sc-bwzfXH.pjOvZ a ')
-console.log(nombres)
-            
+
+            arrPlaylistName.push($(nombres[0]).text())
+            arrPlaylistLinks.push(nombres[0])
             arrPlaylist.push($(nombres[0]).text() +' - '+ $(nombres[1]).text() + ' - ' + nombres[0])
             //console.log(e)    
-            console.log(arrPlaylist)  
+            
         }
         
     }
-
     
 )
+
+console.log(arrPlaylistName.join('\n'))  
+console.log(arrPlaylistLinks.join('\n'))  
+console.log(arrPlaylist.join('\n'))  
