d6e9ac3b96eee3f6f69a64642f09d5397dad3e79
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2023-06-19 14:12:46 -0400

    Update web_youtube-listar-etiquetas.js

--- web_youtube-listar-etiquetas.js
+++ web_youtube-listar-etiquetas.js
@@ -1,3 +1,8 @@
+var script  = document.createElement('script');
+script.src  = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 var etiquetas = ''
 $('div#chip-text').each(function( index ) {
   etiquetas += "'"+$( this ).text() + "', "
