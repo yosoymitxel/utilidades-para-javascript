5000e17f9066b4121dc168af196aeac1371e57b5
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2023-09-14 09:17:45 -0400

    Create web_whatsapp_comprimir-visualmente.js

---
+++ web_whatsapp_comprimir-visualmente.js
@@ -0,0 +1,30 @@
+function dev_js_whatsapp_hacer_mas_chico(){
+    document.querySelectorAll('._2Ts6i._3RGKj').forEach(function(element) {
+      element.style.maxWidth = '30%';
+    });
+    
+    document.querySelectorAll('.Dvjym').forEach(function(element) {
+      element.style.display = 'none';
+    });
+    
+    document.querySelectorAll('._8nE1Y').forEach(function(element) {
+      element.style.padding = '0';
+    });
+    
+    document.querySelectorAll('._2Ts6i._2xAQV').forEach(function(element) {
+      element.style.maxWidth = '70vw';
+    });
+    
+    document.querySelectorAll('._13jwn').forEach(function(element) {
+      element.style.padding = '3px';
+    });
+    
+    document.querySelectorAll('._3OtEr').forEach(function(element) {
+      element.style.margin = '0';
+    });
+    document.querySelector('header.g0rxnol2.ercejckq.cm280p3y.p357zi0d.gndfcl4n.kcgo1i74.ln8gz9je.e8h85j61.emrlamx0.aiput80m.lyvj5e2u.l9g3jx6n.f6ipylw5').style.paddingRight = '0';
+
+}
+
+
+dev_js_whatsapp_hacer_mas_chico()
